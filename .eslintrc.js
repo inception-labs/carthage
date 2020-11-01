a3a138b9fa3577dbfb6c38a431d5bf53122cc5e2
Melek <1846037+meleksomai@users.noreply.github.com>
2020-11-01 18:18:24 +0000

    Adding ESLint to the project

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    env: {
+        node: true
+    },
+    plugins: [
+        '@typescript-eslint',
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+};
